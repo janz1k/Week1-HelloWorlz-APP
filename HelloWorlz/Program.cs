0d76467896ef2bde03f693df5d0621b3c4af76b9
Janis Griezans <janis.griezans@etsnord.ee>
2022-01-31 13:17:41 +0200

    Create HelLo Worlz Program

---
+++ HelloWorlz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorlz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string UserName;
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + UserName);
+
+
+            Console.Read();
+
+        }
+    }
+}
